c4ee630af973874401374de3213004e08ead7f60
Ronan-Cords <rcords@student.fitchburgstate.edu>
2022-05-03 01:51:09 -0400

    Enemy Movement fixed
    Somehow the rigidbody messed up

--- Assets/Scripts/EnemyFollwo.cs
+++ Assets/Scripts/EnemyFollwo.cs
@@ -7,20 +7,22 @@ public class EnemyFollwo : MonoBehaviour
 {
     public float speed = 7.5f;
     private Transform target;
-
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
     }
 
     private void OnTriggerEnter(Collider other)
